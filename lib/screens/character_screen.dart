5fad5067cb0ef981404bc896ef29a4ee7538c271
Sebastian <zuky2435@gmail.com>
2023-12-13 10:51:08 -0500

    Added Json file and functionality in the character screen

--- lib/screens/character_screen.dart
+++ lib/screens/character_screen.dart
@@ -1,15 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:project3/ProductDataModel.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' as rootBundle;
 
-class CharacterScreen extends StatelessWidget{
+class CharacterScreen extends StatefulWidget {
   const CharacterScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  _CharacterScreenState createState() => _CharacterScreenState();
+}
+
+class _CharacterScreenState extends State<CharacterScreen> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Characters'),),
-      body: Center(child: Column( mainAxisAlignment: MainAxisAlignment.center,
-      children: []),
+      appBar: AppBar(
+        title: Text('Characters'),
+      ),
+      body: FutureBuilder<List<ProductDataModel>>(
+        future: readJsonData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text("${snapshot.error}"));
+          } else if (snapshot.hasData) {
+            var items = snapshot.data!;
+            return ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          child: Image.network(
+                            items[index].imageURL!,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.only(bottom: 8),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8, right: 8),
+                                  child: Text(
+                                    items[index].name!,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8, right: 8),
+                                  child: Text(items[index].price!),
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
+  }
+
+  Future<List<ProductDataModel>> readJsonData() async {
+    try {
+      final jsondata =
+          await rootBundle.rootBundle.loadString('jsonfile/productlist.json');
+      final list = json.decode(jsondata) as List<dynamic>;
+
+      return list.map((e) => ProductDataModel.fromJson(e)).toList();
+    } catch (e) {
+      print('Error: $e');
+      return [];
+    }
   }
 }
